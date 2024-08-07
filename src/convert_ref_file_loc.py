f8af72dec6f3dabfa9271694b52cab725745288f
Riley Conroy <rpconroy@ucar.edu>
2024-08-07 10:36:48 -0600

    Added function to convert to remote

---
+++ src/convert_ref_file_loc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import os
+import re
+import pdb
+
+
+
+def main(filename, outfile):
+    ofile = open(outfile, 'w')
+    with open(filename) as fh:
+        for l in fh:
+            match = '\\/gpfs\\/csfs1\\/collections\\/rda\\/data'
+                    #\\/gpfs\\/csfs1\\/collections\\/rda\\/data
+            replacement = 'https:\\/\\/data.rda.ucar.edu'
+            str_output =  l.replace(match, replacement)
+            ofile.write(str_output)
+            print(l)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f'Convert local file to remote file location')
+        print(f'usage: {sys.argv[0]} [filename] [outfile]')
+        exit(1)
+    main(sys.argv[1], sys.argv[2])
