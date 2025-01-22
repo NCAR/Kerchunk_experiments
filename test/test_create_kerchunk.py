a16141e5de4bd9fb255e2a37f60098a96afbdec9
Riley Conroy <rpconroy@ucar.edu>
2025-01-22 15:50:41 -0700

    Add tests

--- test/test_create_kerchunk.py
+++ test/test_create_kerchunk.py
@@ -13,18 +13,17 @@ from kerchunk.combine import MultiZarrToZarr
 def test_find_files():
     regex = r'^.*19:.*$'
     extensions = ['.nc']
-    path = '/gpfs/csfs1/collections/rda/data/d559000/wy1981/198102/'
+    path = '/glade/campaign/collections/rda/data/d559000/wy1981/198102/'
     return create_kerchunk.find_files(path, regex, extensions)
 
 def test_combine():
-    create_kerchunk.process_kerchunk_combine(directory='/gpfs/csfs1/collections/rda/data/d559000/wy1981/198101/', output_directory='.', extensions=[], regex=r"^.*wrf2d.*-01.*$", output_filename="T2_P_198101.json", variables=['T2','P'], dry_run=False)
+    create_kerchunk.process_kerchunk_combine(directory='/glade/campaign/collections/rda/data/d559000/wy1981/198101/', output_directory='.', extensions=[], regex=r"^.*wrf2d.*-01.*$", output_filename="T2_P_198101.json", variables=['T2','P'], dry_run=False)
 
 def test_create_mzz():
-    path = '/gpfs/csfs1/collections/rda/data/d559000/wy1981/198102/wrf3d_d01_1981-02-27_0*'
-    path = '/gpfs/csfs1/collections/rda/data/d633000/e5.oper.an.pl/194007/*_t*.nc'
-    pdb.set_trace()
+    path = '/glade/campaign/collections/rda/data/d559000/wy1981/198102/wrf3d_d01_1981-02-27_0*'
+    path = '/glade/campaign/collections/rda/data/d633000/e5.oper.an.pl/194007/*_t*.nc'
     files = glob.glob(path)
-    print(files)
+    assert(len(files) == 31)
     all_refs = []
     for file in files:
         ref = create_kerchunk.gen_json(file)
@@ -42,6 +41,7 @@ def test_create_mzz():
     output_fname = "combined_kerchunk.json"
     with open(f"{output_fname}", "wb") as f:
         f.write(ujson.dumps(multi_kerchunk).encode())
+    #os.remove(output_fname)
 
 def test_chunks():
     filename = '/gpfs/csfs1/collections/rda/data/d559000/wy1981/198101/wrf2d_d01_1981-01-31_19:00:00.nc'
@@ -56,5 +56,6 @@ def test_get_time_variable():
 
 test_create_mzz()
 files = test_find_files()
+print(files)
 test_combine()
 test_get_time_variable()
