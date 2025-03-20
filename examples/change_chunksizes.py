dec474c661c444696181194fd99166577dd26d4a
Riley Conroy <rpconroy@ucar.edu>
2025-03-20 08:57:09 -0600

    Add method to change chunksizes

---
+++ examples/change_chunksizes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+import xarray
+if len(sys.argv) < 3:
+    print(f'Usage: ./{sys.argv[0]} [filename] [output filename] [var1] [var2] ...')
+    exit(1)
+
+
+filename = sys.argv[1]
+out_filename = sys.argv[2]
+variables = sys.argv[3:]
+
+chunk_sizes = {'chunksizes':[1, 17, 339, 456]}
+ds = xarray.open_dataset(filename)
+encoding = {}
+for v in variables:
+    encoding[v] = chunk_sizes
+
+ds.to_netcdf(out_filename, encoding=encoding)
+
